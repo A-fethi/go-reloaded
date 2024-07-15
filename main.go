407a8487c76bfc4b5992b5108386c49e6d8c3f80
Abderrahmane Fethi <fethiabderrahmane1@gmail.com>
2024-07-15 18:04:03 +0100

    saving files

--- main.go
+++ main.go
@@ -11,75 +11,90 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("Usage: go run main.go <input_file> <output_file>")
+		fmt.Println("Usage: go run . <input_file> <output_file>")
 	} else {
-		content, err := os.ReadFile(os.Args[1])
-		if err != nil {
-			fmt.Println("Error reading file:", err)
-			return
-		}
+		inputFile := os.Args[1]
+		outputFile := os.Args[2]
+		if strings.HasSuffix(inputFile, ".txt") {
+			content, err := os.ReadFile(inputFile)
+			if err != nil {
+				fmt.Println("Error reading file:", err)
+				return
+			}
 
-		str := string(content)
-		splitedStr := strings.Split(str, "\n")
-		var SplitedStr []string
-		for i, line := range splitedStr {
-			words := strings.Split(line, " ")
-			SplitedStr = append(SplitedStr, words...)
-			if i < len(splitedStr)-1 {
-				SplitedStr = append(SplitedStr, "\n")
+			str := string(content)
+			splitedStr := strings.Split(str, "\n")
+			var SplitedStr []string
+			for i, line := range splitedStr {
+				words := strings.Split(line, " ")
+				SplitedStr = append(SplitedStr, words...)
+				if i < len(splitedStr)-1 {
+					SplitedStr = append(SplitedStr, "\n")
+				}
 			}
-		}
-		goreloaded.Atoan(SplitedStr)
-		for i := 0; i < len(SplitedStr); i++ {
-			if SplitedStr[i] == "(hex)" {
-				if i > 0 {
-					SplitedStr[i-1] = goreloaded.HexConv(SplitedStr[i-1])
+			goreloaded.Atoan(SplitedStr)
+			for i := 0; i < len(SplitedStr); i++ {
+				if SplitedStr[i] == "(hex)" {
+					if i > 0 {
+						SplitedStr[i-1] = goreloaded.HexConv(SplitedStr[i-1])
+					}
+					SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
+					i--
+				} else if SplitedStr[i] == "(bin)" {
+					if i > 0 {
+						SplitedStr[i-1] = goreloaded.BinConv(SplitedStr[i-1])
+					}
+					SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
+					i--
+				} else if SplitedStr[i] == "(up)" {
+					if i > 0 {
+						SplitedStr[i-1] = strings.ToUpper(SplitedStr[i-1])
+					}
+					SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
+					i--
+				} else if SplitedStr[i] == "(low)" {
+					if i > 0 {
+						SplitedStr[i-1] = strings.ToLower(SplitedStr[i-1])
+					}
+					SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
+					i--
+				} else if SplitedStr[i] == "(cap)" {
+					if i > 0 {
+						SplitedStr[i-1] = goreloaded.Capitalize(SplitedStr[i-1])
+					}
+					SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
+					i--
+				} else if SplitedStr[i] == "(up," {
+					SplitedStr, i = instancesProcess(SplitedStr, i)
+				} else if SplitedStr[i] == "(low," {
+					SplitedStr, i = instancesProcess(SplitedStr, i)
+				} else if SplitedStr[i] == "(cap," {
+					SplitedStr, i = instancesProcess(SplitedStr, i)
 				}
-				SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
-				i--
-			} else if SplitedStr[i] == "(bin)" {
-				if i > 0 {
-					SplitedStr[i-1] = goreloaded.BinConv(SplitedStr[i-1])
-				}
-				SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
-				i--
-			} else if SplitedStr[i] == "(up)" {
-				if i > 0 {
-					SplitedStr[i-1] = strings.ToUpper(SplitedStr[i-1])
-				}
-				SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
-				i--
-			} else if SplitedStr[i] == "(low)" {
-				if i > 0 {
-					SplitedStr[i-1] = strings.ToLower(SplitedStr[i-1])
-				}
-				SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
-				i--
-			} else if SplitedStr[i] == "(cap)" {
-				if i > 0 {
-					SplitedStr[i-1] = goreloaded.Capitalize(SplitedStr[i-1])
-				}
-				SplitedStr = append(SplitedStr[:i], SplitedStr[i+1:]...)
-				i--
-			} else if SplitedStr[i] == "(up," {
-				SplitedStr, i = instancesProcess(SplitedStr, i)
-			} else if SplitedStr[i] == "(low," {
-				SplitedStr, i = instancesProcess(SplitedStr, i)
-			} else if SplitedStr[i] == "(cap," {
-				SplitedStr, i = instancesProcess(SplitedStr, i)
 			}
-		}
-		var output string
-		for i, str := range SplitedStr {
-			output += str
-			if str != "\n" && i < len(SplitedStr)-1 && SplitedStr[i+1] != "\n" {
-				output += " "
+			var output string
+			for i, str := range SplitedStr {
+				output += str
+				if str != "\n" && i < len(SplitedStr)-1 && SplitedStr[i+1] != "\n" {
+					output += " "
+				}
 			}
+
+			output = goreloaded.Punctuations(output)
+			output = goreloaded.Quotes(output)
+			if strings.HasSuffix(outputFile, ".txt") {
+				err = os.WriteFile(outputFile, []byte(output), 0o644)
+				if err != nil {
+					fmt.Println("Error writing to file:", err)
+					return
+				}
+			} else {
+				fmt.Println("Error: outputFile should be .txt format")
+			}
+			// fmt.Println((output))
+		} else {
+			fmt.Println("Error: inputFile should be .txt format")
 		}
-		
-		output = goreloaded.Punctuations(output)
-		output = goreloaded.Quotes(output)
-		fmt.Println((output))
 	}
 }
 
